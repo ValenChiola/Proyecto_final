d71271818cc2e218db2bc5c685b07e2a33c421af
ValenChiola <tinochiola06@gmail.com>
2020-11-16 19:59:25 -0300

    primer commit

---
+++ src/main/java/com/clasifacil/ClasifacilApplication.java
@@ -0,0 +1,13 @@
+package com.clasifacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasifacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasifacilApplication.class, args);
+	}
+
+}
