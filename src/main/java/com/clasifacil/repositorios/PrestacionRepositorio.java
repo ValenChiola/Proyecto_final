3c1b776b373f7caa224f75ee2d74805655650b34
ValenChiola <tinochiola06@gmail.com>
2020-11-17 20:09:32 -0300

    usuario y prestacion hecho

--- src/main/java/com/clasifacil/repositorios/PrestacionRepositorio.java
+++ src/main/java/com/clasifacil/repositorios/PrestacionRepositorio.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.clasifacil.repositorios;
 
-/**
- *
- * @author Estela
- */
-public interface PrestacionRepositorio {
+import com.clasifacil.entidades.Prestacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrestacionRepositorio extends JpaRepository<Prestacion, String>{
     
 }
