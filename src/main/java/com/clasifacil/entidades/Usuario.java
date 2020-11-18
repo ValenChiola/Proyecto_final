354679c9926e75401ef9a3e02f4f15ab41ab1ed0
Fabricio Coria <coriafabri37@gmail.com>
2020-11-18 20:23:00 -0300

    arreglo voto

--- src/main/java/com/clasifacil/entidades/Usuario.java
+++ src/main/java/com/clasifacil/entidades/Usuario.java
@@ -1,4 +1,8 @@
 package com.clasifacil.entidades;
 
 public class Usuario {    
+
+    public Object getDni() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
