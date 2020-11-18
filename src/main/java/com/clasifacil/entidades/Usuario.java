0562a5975632623ed583a9f7c9c77b2f47bd03df
Fabricio Coria <coriafabri37@gmail.com>
2020-11-18 19:43:12 -0300

    arreglo voto y zona

--- src/main/java/com/clasifacil/entidades/Usuario.java
+++ src/main/java/com/clasifacil/entidades/Usuario.java
@@ -1,9 +1,4 @@
 package com.clasifacil.entidades;
 
-public class Usuario {
-
-    public Object getID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+public class Usuario {    
 }
