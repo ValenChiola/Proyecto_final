3c3d478350b3cd0ebe6b021a9358d930eaabf6ce
BelenGiunta <velu.masami.megumi@gmail.com>
2020-11-18 18:40:33 -0300

    Actualizacion de foto service

--- src/main/java/com/clasifacil/service/FotoService.java
+++ src/main/java/com/clasifacil/service/FotoService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.stereotype.Service;
 
 @Service
 public class FotoService {
@@ -20,6 +21,45 @@ public class FotoService {
     private FotoRepositorio fotoRepositorio;
 
     public Foto guardar(MultipartFile archivo) throws Error {
+
+        if (archivo != null) {
+            try {
+
+                Foto foto = new Foto();
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+
+                return fotoRepositorio.save(foto);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    public Foto actulizar(String idFoto, MultipartFile archivo) throws Error {
+
+        if (archivo != null) {
+            try {
+
+                Foto foto = new Foto();
+                if (idFoto != null) {
+                    Optional<Foto> respuesta = fotoRepositorio.findById(idFoto);
+                    if (respuesta.isPresent()) {
+                        foto = respuesta.get();
+                    }
+
+                }
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+
+                return fotoRepositorio.save(foto);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
         return null;
     }
 }
