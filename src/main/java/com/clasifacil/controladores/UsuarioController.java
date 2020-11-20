ec6aee00d295348553fb8c755a126f700419ee4b
Fabricio Coria <coriafabri37@gmail.com>
2020-11-20 18:28:59 -0300

    modificarUsuario terminado

--- src/main/java/com/clasifacil/controladores/UsuarioController.java
+++ src/main/java/com/clasifacil/controladores/UsuarioController.java
@@ -94,4 +94,39 @@ public class UsuarioController {
 
         return "redirect:/usuario/inicio";
     }
+    
+    @GetMapping("/modificar")
+    public String modificarUsuario(ModelMap modelo) {
+        List<Zona> zonas = zonaRepositorio.findAll();
+        modelo.put("zonas", zonas);
+        return "modificar-usuario.html";
+    }
+    
+    @PostMapping("/actualizar")
+    public String modificarUsuario(ModelMap modelo, @RequestParam String dni,
+            @RequestParam String nombre, @RequestParam String apellido, @RequestParam String mail,
+            @RequestParam String telefono, @RequestParam String clave1, @RequestParam String clave2,
+            @RequestParam String idZona) throws Error {
+
+        try {
+            usuarioService.modificarUsuario(dni, nombre, apellido, mail, telefono, clave1, clave2, idZona);
+
+        } catch (Error e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("dni", dni);
+            modelo.put("nombre", nombre);
+            modelo.put("apellido", apellido);
+            modelo.put("mail", mail);
+            modelo.put("telefono", telefono);
+            modelo.put("clave1", clave1);
+            modelo.put("clave2", clave2);
+            modelo.put("idZona", idZona);
+
+            return modificarUsuario(modelo);
+
+        }
+
+        modelo.put("exito", "Te has registrado existosamente");
+        return "redirect:/usuario/inicio";
+    }
 }
