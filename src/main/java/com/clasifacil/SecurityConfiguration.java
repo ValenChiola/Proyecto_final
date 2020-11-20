56d42cf25de9f8b4817ca8b4d9d5ba4f9bcfed30
ValenChiola <tinochiola06@gmail.com>
2020-11-20 16:52:29 -0300

    Arreglo de vistas, services y controladores

--- src/main/java/com/clasifacil/SecurityConfiguration.java
+++ src/main/java/com/clasifacil/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.clasifacil;
 
+import com.clasifacil.service.PrestadorService;
 import com.clasifacil.service.UsuarioService;
-import static javafx.scene.input.KeyCode.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
