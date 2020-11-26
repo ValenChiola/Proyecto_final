9a0dc263273b8b438810c55df1debb7c2fd61d78
ValenChiola <tinochiola06@gmail.com>
2020-11-26 19:05:30 -0300

    Todo lo del voto hecho

--- src/main/java/com/clasifacil/repositorios/VotoRepositorio.java
+++ src/main/java/com/clasifacil/repositorios/VotoRepositorio.java
@@ -7,6 +7,8 @@ package com.clasifacil.repositorios;
 
 import com.clasifacil.entidades.Voto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,5 +17,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface VotoRepositorio extends JpaRepository<Voto, String> {
-    
+ 
+    @Query(value="DELETE FROM Voto v WHERE v.prestador_cuit LIKE :cuit",nativeQuery=true)
+    public void eliminarVotoPorPrestador(@Param("cuit") String cuit);
 }
