5da1bc86f217a73c9ac81ef233711ae218df80f2
ValenChiola <tinochiola06@gmail.com>
2020-12-03 18:09:56 -0300

    rreglo de votoService

--- src/main/java/com/clasifacil/service/VotoService.java
+++ src/main/java/com/clasifacil/service/VotoService.java
@@ -15,24 +15,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VotoService {
-    
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
+
     @Autowired
     private PrestadorRepositorio prestadorRepositorio;
-    
+
     @Autowired
     private NotificacionService notificacionService;
-    
+
     @Autowired
     private VotoRepositorio votoRepositorio;
-    
-    public void votar(String id, String CUIT,Valoraciones val){
-        
+
+    public void votar(String id, String CUIT, Valoraciones val) {
+
         Voto voto = new Voto();
         int puntaje;
-        
+
         switch (val.toString()) {
             case "MALA":
                 puntaje = 1;
@@ -46,59 +46,69 @@ public class VotoService {
             case "MUY_BUENA":
                 puntaje = 4;
                 break;
-            default:
+            case "EXCELENTE":
                 puntaje = 5;
                 break;
+                
+            default:
+                puntaje = 0;
         }
-        
+
         Optional<Usuario> respuesta2 = usuarioRepositorio.findById(id);
         Optional<Prestador> respuesta = prestadorRepositorio.findById(CUIT);
-                
+
         if (respuesta.isPresent()) {
             Prestador prestador1 = respuesta.get();
-            
+
             if (prestador1.getCuit().equals(CUIT)) {
                 voto.setPrestador(prestador1);
-            }else {
+            } else {
                 throw new Error("No tiene permisos para realizar la operación solicitada");
             }
 
-            if(prestador1.getValoracion() == null){
+            if (prestador1.getValoracion() == null) {
                 prestador1.setValoracion(puntaje);
-            }else{
-                prestador1.setValoracion(Math.toIntExact((prestador1.getValoracion()+puntaje)/2));
+            } else {
+                
+                float suma = (prestador1.getValoracion() + puntaje);
+                float promedio = suma/2;
+                
+                if (promedio == 4.5) {
+                    prestador1.setValoracion(5);
+                } else {
+                    prestador1.setValoracion((int)promedio);
+                }
             }
-            
+
             prestadorRepositorio.save(prestador1);
-        }else {
+        } else {
             throw new Error("No existe un prestador vinculado a ese identificador");
         }
-        
+
         if (respuesta2.isPresent()) {
             Usuario usuario1 = respuesta2.get();
-            
+
             if (usuario1.getDni().equals(id)) {
                 voto.setUsuario(usuario1);
-            }else {
+            } else {
                 throw new Error("No tiene permisos para realizar la operación solicitada");
             }
-        }else {
+        } else {
             throw new Error("No existe un usuario vinculado a ese identificador");
         }
-        
+
         votoRepositorio.save(voto);
         notificacionService.enviarPorVoto(id, CUIT);
     }
-    
+
     @Transactional
-    public void eliminarVotoPorPrestador(String cuit){
-        
+    public void eliminarVotoPorPrestador(String cuit) {
+
         List<Voto> votos = votoRepositorio.listarVotosPorPrestador(cuit);
-        
-        if(!votos.isEmpty()){
+
+        if (!votos.isEmpty()) {
             votoRepositorio.eliminarVotoPorPrestador(cuit);
         }
     }
-    
-    
+
 }
